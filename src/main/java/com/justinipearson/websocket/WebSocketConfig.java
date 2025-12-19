e3eacbdae026e3e9717888b55bec646d72406b45
stockboy518 <aiengineerjp@gmaial.com>
2025-12-19 14:25:14 -0800

    configuring inbound and outbound channels

--- src/main/java/com/justinipearson/websocket/WebSocketConfig.java
+++ src/main/java/com/justinipearson/websocket/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.justinipearson.websocket;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -11,7 +12,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         //Client Connects here
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*");
     }
 
     @Override
@@ -20,5 +22,15 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.setApplicationDestinationPrefixes("/app");
         //Prefix for messages from server to client
         registry.enableSimpleBroker("/topic");
+    }
+    //Add multi threading for inbound messages
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.taskExecutor().corePoolSize(10).maxPoolSize(20).queueCapacity(100);
+    }
+    //Add multi threading for inbound messages
+    @Override
+    public void configureClientOutboundChannel(ChannelRegistration registration) {
+        registration.taskExecutor().corePoolSize(10).maxPoolSize(20).queueCapacity(100);
     }
 }
