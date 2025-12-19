91f6de0f299604acb1bf4a9ad7630359ccf9a64f
stockboy518 <aiengineerjp@gmaial.com>
2025-12-18 16:48:21 -0800

    basic single thread websocket

--- src/main/java/com/justinipearson/websocket/WebSocketConfig.java
+++ src/main/java/com/justinipearson/websocket/WebSocketConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
+//Websocket configure
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
